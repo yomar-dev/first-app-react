2e9e8a039de0b53b4d1ac392313967077538cdca
Yomar Miranda <mirandayomar@gmail.com>
2018-06-20 10:30:37 -0500

    Eliminamos el componente Navigation y creamos la navegación directamente en el componente App.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Navigation from './components/Navigation';
-
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Navigation titulo="Mi primera App con React." />
+                <nav className="navbar navbar-dark bg-dark">
+                    <a href="#" className="text-white">Task</a>
+                </nav>
                 <img src={logo} className="App-logo" alt="logo" />
             </div>
         );
