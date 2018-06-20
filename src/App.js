1c41e09734480cf5b9eb04a1979f25f17a5f72ed
Yomar Miranda <mirandayomar@gmail.com>
2018-06-20 11:55:33 -0500

    Mostrando los datos de nuestra API en nuestra App.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { todos } from './todos.json';
+
 class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            todos
+        }
+    }
+
     render() {
+        const todos = this.state.todos.map((todo, i) => {
+            return(
+                <div className="col-md-4 mt-4">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3>{todo.title}</h3>
+                            <span className="badge badge-pill badge-danger ml-2">
+                                {todo.priority}
+                            </span>
+                        </div>
+                        <div className="card-body">
+                            <p>{todo.description}</p>
+                            <p><strong>{todo.responsible}</strong></p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+
         return (
             <div className="App">
                 <nav className="navbar navbar-dark bg-dark">
                     <a href="#" className="text-white">Task</a>
                 </nav>
+
+                <div className="container">
+                    <div className="row mt-4">
+                        { todos }
+                    </div>
+                </div>
+
                 <img src={logo} className="App-logo" alt="logo" />
             </div>
         );
